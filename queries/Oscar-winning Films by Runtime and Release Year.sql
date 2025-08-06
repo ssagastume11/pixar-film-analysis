cab8ea4188d0b28c464c37d2b71b6361d9fd0fa0
ssagastume11 <sagassergio123@gmail.com>
2025-08-06 11:55:04 -0400

    Initial commit with Pixar Film Analysis project files

--- queries/Oscar-winning Films by Runtime and Release Year.sql
+++ queries/Oscar-winning Films by Runtime and Release Year.sql
@@ -1,1 +1,17 @@
-
+SELECT
+  f.film,
+  f.release_year,
+  f.run_time,
+  f.film_rating,
+  a.award_type,
+  a.status
+FROM
+  `plenary-ability-463920-b3.pixar_films_data.film_analysis_cleaned` f
+JOIN
+  `plenary-ability-463920-b3.pixar_films_data.academy_awards_cleaned` a
+ON
+  f.film = a.film
+WHERE
+  a.status = 'Won'
+ORDER BY
+  f.release_year ASC, f.run_time DESC;
