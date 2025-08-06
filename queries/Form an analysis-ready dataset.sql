42c44d74de06910cfed0cd965d3d7349587c5e97
ssagastume11 <sagassergio123@gmail.com>
2025-08-06 11:54:36 -0400

    Initial commit with Pixar Film Analysis project files

--- queries/Form an analysis-ready dataset.sql
+++ queries/Form an analysis-ready dataset.sql
@@ -1,1 +1,9 @@
-
+-- 1. Check for missing values in the pixar_films table
+SELECT 
+  COUNT(*) AS total_rows,
+  COUNTIF(film IS NULL) AS missing_film,
+  COUNTIF(release_date IS NULL) AS missing_release_date,
+  COUNTIF(run_time IS NULL) AS missing_run_time,
+  COUNTIF(film_rating IS NULL) AS missing_film_rating,
+  COUNTIF(plot IS NULL) AS missing_plot
+FROM `plenary-ability-463920-b3.pixar_films_data.pixar_films`;
